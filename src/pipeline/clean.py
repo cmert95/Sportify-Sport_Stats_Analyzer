cd382c5bcdefc8e7966559998128510c0f1967d7
cmert95 <cm.timurtas@outlook.com>
2025-05-17 15:14:48 +0200

    refactor(pipeline): rename files and add logger

--- src/pipeline/clean.py
+++ src/pipeline/clean.py
@@ -1,11 +1,16 @@
 from clean_bronze_data import clean_bronze_data
+from utils.logger import setup_logger
 from utils.spark_session import get_spark_session
+
+logger = setup_logger(__name__, log_name="clean_data")
 
 
 def main():
     spark = get_spark_session("Clean Bronze Data")
+    logger.info("Spark session started")
     clean_bronze_data(spark)
     spark.stop()
+    logger.info("Spark session stopped")
 
 
 if __name__ == "__main__":
--- src/pipeline/combine.py
+++ src/pipeline/combine.py
@@ -1,11 +1,16 @@
 from combine_raw_json import combine_raw_json_to_bronze
+from utils.logger import setup_logger
 from utils.spark_session import get_spark_session
+
+logger = setup_logger(__name__, log_name="combine_seasons")
 
 
 def main():
     spark = get_spark_session("Combine Raw JSON to Bronze")
+    logger.info("Spark session started")
     combine_raw_json_to_bronze(spark)
     spark.stop()
+    logger.info("Spark session stopped")
 
 
 if __name__ == "__main__":
